9a1bc97178777472b074afcfc23fb5b4059aa7cd
Paul Joe Koshy <paul.joe@kalvium.community>
2024-12-05 10:20:14 +0530

    port number

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // Use environment variable for port, with a default fallback
-const port = process.env.PORT || 3000;
+const port =  3000;
 
 app.get('/ping', (req, res) => {
     res.send('pong');
